e133bd1fa9da46ccc7fe3d22046b81797b1400ff
aProject2021 <aliciaproyectos2021@gmail.com>
2025-03-30 06:53:56 -0500

    Menu movil

--- asset/js/main_1.js
+++ asset/js/main_1.js
@@ -27,13 +27,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
     const menuToggle = document.querySelector(".menu-toggle");
+    const closeMenu = document.querySelector(".close-menu");
     const navLinks = document.querySelector(".nav-links");
+    const overlay = document.querySelector(".menu-overlay");
     const dropdown = document.querySelector(".dropdown");
 
+    // Abrir menú
     menuToggle.addEventListener("click", function () {
         navLinks.classList.toggle("active");
+        overlay.classList.toggle("active");
     });
 
+    // Cerrar menú al hacer clic en el botón de cerrar
+    closeMenu.addEventListener("click", function () {
+        navLinks.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+
+    // Cerrar menú al hacer clic fuera (overlay)
+    overlay.addEventListener("click", function () {
+        navLinks.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+
+    // Desplegable de "Sectores"
     dropdown.addEventListener("click", function () {
         this.classList.toggle("active");
     });
